a5b575cdedfb20a9c590a18230b8e8615c2a8511
BlackBrocolli <201111011@mhs.stiki.ac.id>
2024-03-08 09:14:14 +0700

    Update OutlinedTextField

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -26,8 +27,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
@@ -51,6 +57,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember {
+        mutableStateOf(0.01)
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -60,11 +76,12 @@ fun UnitConverter() {
         Text("Unit Converter")
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
-            value = "Enter Value",
+            value = inputValue,
             onValueChange = {
-                // here goes what should happen when the value change
+                inputValue = it
             },
-            label = { Text("Label") }
+            label = { Text("Enter Value") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
         )
         Spacer(modifier = Modifier.height(16.dp))
         Row {
@@ -75,11 +92,11 @@ fun UnitConverter() {
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
                 DropdownMenu(
-                    expanded = false,
+                    expanded = iExpanded,
                     onDismissRequest = { /*TODO*/ },
                     modifier = Modifier
                         .verticalScroll(rememberScrollState())
-                        .heightIn(max = 200.dp)
+                        .heightIn(max = 150.dp)
                 ) {
                     DropdownMenuItem(
                         text = { Text("Centimeters") },
@@ -106,11 +123,11 @@ fun UnitConverter() {
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
                 }
                 DropdownMenu(
-                    expanded = false,
+                    expanded = oExpanded,
                     onDismissRequest = { /*TODO*/ },
                     modifier = Modifier
                         .verticalScroll(rememberScrollState())
-                        .heightIn(max = 200.dp)
+                        .heightIn(max = 150.dp)
                 ) {
                     DropdownMenuItem(
                         text = { Text("Centimeters") },
